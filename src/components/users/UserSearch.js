cf0702a3d88da1e7ac9d4ea9376233830abb8503
Jatin Yadav <Jatin.Yadav@wuh-group.com>
2024-01-31 21:16:53 +0530

    Minor Changes

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -28,14 +28,13 @@ const UserSearch = () => {
                 type: "GET_USER",
                 payload: users,
             });
-            setInputText("");
         }
     };
 
     return (
         <>
             <h1 className="text-center text-5xl mb-10">
-                <div className="text-primary" >
+                <div className="text-primary mt-10" >
                     Search Github Users around the Globe
                 </div>
             </h1>
@@ -57,7 +56,7 @@ const UserSearch = () => {
                                     type="submit"
                                     className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
                                 >
-                                    Go
+                                    Search
                                 </button>
                             </div>
                         </div>
